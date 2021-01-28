df983d19e2c834472a4733b1deb0b1ea67ad5d4f
unknown <dimfreelancerdn@gmail.com>
2021-01-28 13:15:36 +0300

    Lesson01 готовим урок 1

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * Lesson01
+ */
+
+let money, 
+    income, 
+    addExpenses, 
+    deposit, 
+    mission, 
+    period;
+
+
+alert('Hello, world!!! JS 17');
+
+console.log('Hello, world!!!');
