674d1ab79587c43fc0087e45c3d4acc26818a0c2
Dim <dimendroider@gmail.com>
2021-01-29 21:50:31 +0300

    Lesson02 feat init variables and output to console

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 let money = 67000,      //доход за месяц
     income = 'фриланс', //строка с доп доходом
-    addExpenses = '',   //строка через ,запятую, доп расходы
+    addExpenses = 'такси, комуналка',   //строка через ,запятую, доп расходы
     deposit = true,     //есть ли депозит
     mission = 100000,   //цель накопить сумму
     period = 12;        //за какой период
@@ -17,13 +17,13 @@ console.log(typeof deposit);
 
 console.log(addExpenses.length);
 
-console.log('Период равен ' + period + 'месяцев');
-console.log('Цель заработать ' + mission + 'рублей/долларов/гривен/юани');
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+
 
 console.log(addExpenses.toLowerCase());
 
 
 let budgetDay = money / 30; //дневной бюджет
-
 
 console.log(budgetDay);
