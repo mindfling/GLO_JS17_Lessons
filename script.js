53ad6dbae548c07471a229ac28fa0cbb1e34ebff
Dim <dimendroider@gmail.com>
2021-01-29 21:30:36 +0300

    Lesson02 begin to do task

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 'use strict';
 /**
- * Lesson01
+ * Lesson02
  */
 
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money,          //доход за месяц
+    income,         //строка с доп доходом
+    addExpenses = '',//строка через ,,, доп расходы
+    deposit,        //есть ли депозит
+    mission,        //цель накопить сумму
+    period;         //за какой период
 
 
-alert('Hello, world!!! JS 17');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Hello, world!!!');
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + 'месяцев');
+console.log('Цель заработать ' + mission + 'рублей/долларов/гривен/юани');
+
+console.log(addExpenses.toLowerCase());
+
+
+let budgetDay = money / 30; //дневной бюджет
+
+
+console.log(budgetDay);
