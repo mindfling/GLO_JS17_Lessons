909aca0f6a9cdd0aaeede95417cce0ee60f44299
Dimk <dimendroider@gmail.com>
2021-02-01 14:14:34 +0300

    Lesson01 feat add new comments

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
  * Lesson01
  */
 
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money,          //число Доход за месяц
+    income,         //строка с дополнительными доходом
+    addExpenses,    //строка с перечислением дополнительных расходов 
+    deposit,        //любое булево значени
+    mission,        //любое число Какую сумму хотите накопить
+    period;         //число от 1 до 12 месяцев
 
 
 alert('Hello, world!!! JS 17');
 
-console.log('Hello, world!!!');
+console.log('Hello, world!!! lets begin');
